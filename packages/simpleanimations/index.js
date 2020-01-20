13ea66950ef16b2c820c910708732aca131b5808
XCRON <58081288+XCRON-DEV@users.noreply.github.com>
2020-01-20 23:32:56 +0100

    release v.1

---
+++ packages/simpleanimations/index.js
@@ -0,0 +1,19 @@
+mp.events.add("playAnimationEvent", (player, animGroup, animType, speed, moveNumber, milSec) => {
+	if(!mp.players.exists(player)) return;
+	if(player.vehicle) return;
+	player.playAnimation(animGroup, animType, speed, moveNumber);
+	if(milSec && milSec != -1) {
+		player.call("quitAnimationAfter", [player, parseInt(milSec)]);
+	}
+});
+
+mp.events.add("server:animations:facial", (player, animName, animDict) => {
+	if(!mp.players.exists(player)) return;
+	player.playFacialAnim(animName, animDict);
+});
+
+mp.events.add("stopAnimation", (player) => {
+	if(!mp.players.exists(player)) return;
+	if(player.vehicle) return;
+	player.stopAnimation();
+});
